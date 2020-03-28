dba3c17d71c083ba758fd23f5caaea903bd29f61
Rahul Tandon <jesuisrahult@gmail.com>
2020-03-28 23:46:03 +0530

    random message

--- messages.js
+++ messages.js
@@ -1,6 +1,6 @@
 msgs = {
     wash: 'Everybody @here, go and wash your hands right now!',
-    kids: ['Ravit'],
+    kids: [],
     morning: 'Good morning bois! \n I hope all of you are keeping healthy and safe, and have had a hearty breakast! C\'mon bois let\'s get to work!',
     finale: 'Ah! My final message is here my padawans! I hope that ALL of YOU who participated during these past 40DAYS have learnt A LOT, and I hope that you enjoyed a lot. Always remember\n\n \|\|THIS IS NOIDA\|\|',
     get evening() {
